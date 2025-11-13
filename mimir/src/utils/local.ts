325f02b116375ea2ef9533e76d485d545821489f
Mate Csornyei <mate@csornyei.com>
2025-11-14 00:02:15 +0100

    mimir: basepath 2 - api

--- mimir/src/utils/local.ts
+++ mimir/src/utils/local.ts
@@ -2,11 +2,11 @@ import axios from "axios";
 import { HealthResponse, PowerAction } from "@/utils/types";
 
 export const getPcHealth: () => Promise<HealthResponse> = async () => {
-  const response = await axios.get("/api/pc");
+  const response = await axios.get("/mimir/api/pc");
   return response.data;
 };
 
 export const sendPowerCommand = async (action: PowerAction) => {
-  const response = await axios.post("/api/pc", { action });
+  const response = await axios.post("/mimir/api/pc", { action });
   return response.data;
 };
